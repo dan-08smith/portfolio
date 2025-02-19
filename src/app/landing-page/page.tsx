a9302c6e97aa8b63e871227ba898378df7fd2855
dan-08smith <157528938+dan-08smith@users.noreply.github.com>
2025-02-19 16:54:26 +0000

    Improve Mobile Formatting

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -1,5 +1,10 @@
 import LinkIcons from "@/components/LinkIcons";
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Daniel Smith | Landing Page",
+};
 
 export default function LandingPage() {
   return (
@@ -9,20 +14,19 @@ export default function LandingPage() {
         <h1 className="mt-4 text-4xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
           Daniel Smith
         </h1>
-        <p className="mt-6 text-lg text-gray-500 sm:text-xl">
+
+        <p className="mt-6 text-lg text-gray-500 sm:text-xl max-w-xs sm:max-w-none mx-auto">
           Welcome to my landing page, where would you like to go?
         </p>
 
+
         <div className="mt-10">
-          <Link
-            href="/"
-            className="rounded-md bg-blue-600 px-4 py-2 text-sm font-semibold text-white shadow hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            My Website
-          </Link>
+          <Link href="/"
+            className="rounded-md bg-blue-600 px-4 py-2 text-ml font-semibold text-white shadow hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >My Website</Link>
         </div>
 
-        <div className="mt-10 flex items-center justify-center space-x-8">
+        <div className="mt-10 flex flex-col sm:flex-row items-center justify-center space-y-6 sm:space-x-8 sm:space-y-0">
           <LinkIcons img="/icons/linkedin.svg" alt="LinkedIn Icon" href="https://www.linkedin.com/in/dansmith08/" text="My LinkedIn" showArrow={true}/>
           <LinkIcons img="/icons/github.svg" alt="GitHub Icon" href="https://github.com/dan-08smith" text="My GitHub" showArrow={true}/>  
           <LinkIcons img="/icons/send.svg" alt="Send Icon" href="mailto:daniel@d-smith.co.uk" text="Email Me" showArrow={true}/>   
